2cc330f5e1a74efdffbe0960ac5e91da29ab6b91
Konekoya <tank158r5155@yahoo.com.tw>
2016-04-01 17:28:59 +0800

    Set up Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,55 @@
 var gulp = require('gulp');
-var args = require('yargs').argv;
-var config = require('./gulp.config')();
 var $ = require('gulp-load-plugins')({lazy: true});
-var del = require('del');
 var cssnano = require('cssnano');
-var mainBowerFiles = require('main-bower-files');
+var runSequence = require('run-sequence');
 
+// Pathes
+var src = './src/';
+var config = {
+  build: './build/',
+  html: src + 'index.html',
+  scss: src + 'scss/style.scss',
+  js: src + 'js/scripts.js'
+};
+
+gulp.task('htmls', function() {
+  console.log('Moving index.html to build folder');
+  return gulp
+    .src(config.html)
+    .pipe(gulp.dest(config.build))
+});
+
+gulp.task('styles', function() {
+  console.log('Compiling SCSS --> CSS');
+  return gulp
+    .src(config.scss)
+    .pipe($.sassGlob())
+    .pipe($.sass().on('error', $.sass.logError))
+    .pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
+    .pipe($.postcss([
+      cssnano()
+    ]))
+    .pipe($.concat('app.css'))
+    .pipe(gulp.dest(config.build + 'css'));
+});
+
+gulp.task('scripts', function() {
+  console.log('Analyzing source with JSHint and JSCS');
+  return gulp
+    .src(config.js)
+    .pipe($.plumber())
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.uglify())
+    .pipe($.concat('app.js'))
+    .pipe(gulp.dest(config.build + 'js'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(config.js, ['scripts']);
+  gulp.watch(config.scss, ['styles']);
+});
+
+gulp.task('default', function() {
+  runSequence('htmls', 'styles', 'scripts', 'watch');
+});
