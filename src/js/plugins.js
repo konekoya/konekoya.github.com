c2d7ccf709dccb92b8a4bc499cadedb77d0d81ce
Konekoya <tank158r5155@yahoo.com.tw>
2016-05-02 16:37:18 +0800

    Tweak fullpage.js settings

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -1,13 +1,38 @@
-(function($) {
+var HEYJOSHUA = (function($) {
+  'use strict';
 
-  // no need to include document.ready function here,
-  // since we included the script before closing body tag
-  $('#fullpage').fullpage({
-    css3: true,
-    fitToSection: true
-  });
+  function moveToIntro(el) {
+    if (typeof el === 'string' && el.length > 0) {
+      var $el = $(el);
+      $el.on('click', function() {
+        $.fn.fullpage.moveTo('intro');
+      });
+    }
+  }
 
-  // initialize wow.js
-  new WOW().init();
+  function init() {
+    // no need to include document.ready function here,
+    // since we included the script before closing body tag
+    $('#fullpage').fullpage({
+      css3: true,
+
+      // easing tool - https://matthewlein.com/ceaser/
+      easingcss3: 'cubic-bezier(0.895, 0.030, 0.685, 0.220)', 
+      fitToSection: true
+    });
+
+
+    moveToIntro('.avatar__img');
+  }
+
+  // public APIs
+  return {
+    init: init
+  };
 
 }(jQuery));
+
+
+$(document).ready(function() {
+  HEYJOSHUA.init();
+});
