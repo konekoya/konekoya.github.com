c6f1e088905b321c1367a579108d403faf827186
konekoya <tank158r5155@yahoo.com.tw>
2016-09-27 06:30:35 +0800

    Tweak gulp set up and change webserver

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,2 +1,73 @@
-"use strict";!function(){var t=angular.module("githubViewer",[]),e=function(t,e){var r=function(r){t.user=r.data,e.get(t.user.repos_url).then(o,n)},o=function(e){t.repos=e.data},n=function(e){t.error="Could not fetch the data."};e.get("https://api.github.com/users/konekoya").then(r,n),t.message="Hey Joshua!",t.repoSortOrder="-stargazers_count"};t.controller("MainController",["$scope","$http",e])}();
-"use strict";var JOSHUA=function(e){function n(n){if("string"==typeof n&&n.length>0){var t=i.querySelector(n);t&&t.addEventListener("click",function(){e.fn.fullpage.moveTo("intro")},!1)}}function t(){e("#fullpage").fullpage({menu:"#navigation",navigation:!0,navigationTooltips:["Home sweet home","Yep, A lllllittle About Me"],css3:!0,easingcss3:"cubic-bezier(0.895, 0.030, 0.685, 0.220)",fitToSection:!0}),n(".avatar__mouse")}var i=document;return{init:t}}(jQuery);$(document).ready(function(){JOSHUA.init()});
+'use strict';
+
+(function () {
+  'use strict';
+
+  var app = angular.module('githubViewer', []);
+
+  var MainController = function MainController($scope, $http) {
+
+    var onUserComplete = function onUserComplete(response) {
+      $scope.user = response.data;
+      $http.get($scope.user.repos_url).then(onRepos, onError);
+    };
+
+    var onRepos = function onRepos(response) {
+      $scope.repos = response.data;
+    };
+
+    var onError = function onError(error) {
+      $scope.error = 'Could not fetch the data.';
+    };
+
+    $http.get('https://api.github.com/users/konekoya').then(onUserComplete, onError);
+
+    $scope.message = 'Hey Joshua!';
+    $scope.repoSortOrder = '-stargazers_count';
+  };
+
+  app.controller('MainController', ['$scope', '$http', MainController]);
+})();
+'use strict';
+
+var JOSHUA = function ($) {
+  'use strict';
+
+  var doc = document;
+
+  function moveToIntro(el) {
+    if (typeof el === 'string' && el.length > 0) {
+      var trigger = doc.querySelector(el);
+
+      if (trigger) {
+        trigger.addEventListener('click', function () {
+          $.fn.fullpage.moveTo('intro');
+        }, false);
+      }
+    }
+  }
+
+  function init() {
+    $('#fullpage').fullpage({
+      menu: '#navigation',
+      navigation: true,
+      navigationTooltips: ['Home sweet home', 'Yep, A lllllittle About Me'],
+      css3: true,
+
+      // easing tool - https://matthewlein.com/ceaser/
+      easingcss3: 'cubic-bezier(0.895, 0.030, 0.685, 0.220)',
+      fitToSection: true
+    });
+
+    moveToIntro('.avatar__mouse');
+  }
+
+  // public APIs
+  return {
+    init: init
+  };
+}(jQuery);
+
+$(document).ready(function () {
+  JOSHUA.init();
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,17 @@
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')({ lazy: true });
-const cssnano = require('cssnano');
 const runSequence = require('run-sequence');
 const gutil = require('gulp-util');
 
 // Pathes
 const src = './src/';
 const config = {
+  root: './',
   build: './build/',
-  dev: './dev/',
-  html: './index.html',
   scss: src + '/scss/**/*.scss',
   js: src + 'js/**/*.js',
   images: src + 'images/**/*'
 };
-
-gulp.task('styles:dev', () => {
-  log('Compiling SCSS --> CSS');
-  return gulp
-    .src(config.scss)
-    .pipe($.sassGlob())
-    .pipe($.sass().on('error', $.sass.logError))
-    .pipe($.autoprefixer({ browsers: ['last 2 version', '> 5%'] }))
-    .pipe(gulp.dest(config.dev + 'css'))
-    .pipe($.connect.reload());
-});
-
 
 gulp.task('styles', () => {
   log('Compiling SCSS --> CSS');
@@ -34,27 +20,10 @@ gulp.task('styles', () => {
     .pipe($.sassGlob())
     .pipe($.sass().on('error', $.sass.logError))
     .pipe($.autoprefixer({ browsers: ['last 2 version', '> 5%'] }))
-    .pipe($.postcss([
-      cssnano()
-    ]))
     .pipe($.concat('styles.css'))
-    .pipe(gulp.dest(config.dev + 'css'))
-    .pipe($.connect.reload());
+    .pipe(gulp.dest(config.build + 'css'));
 });
 
-gulp.task('scripts:dev', () => {
-  log('Analyzing source with JSHint');
-  return gulp
-    .src(config.js)
-    .pipe($.plumber())
-    .pipe($.babel({
-        presets: ['es2015']
-    }))
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe(gulp.dest(config.build + 'js'))
-    .pipe($.connect.reload());
-});
 
 gulp.task('scripts', () => {
   log('Analyzing source with JSHint');
@@ -66,69 +35,37 @@ gulp.task('scripts', () => {
     }))
     .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe($.uglify())
     .pipe($.concat('scripts.js'))
-    .pipe(gulp.dest(config.dev + 'js'))
-    .pipe($.connect.reload());
-});
-
-gulp.task('images:dev', () => {
-  log('Moving images source to build directory');
-  return gulp
-    .src(config.images)
-    .pipe(gulp.dest(config.dev + 'images'))
-    .pipe($.connect.reload());
+    .pipe(gulp.dest(config.build + 'js'));
 });
 
 gulp.task('images', () => {
   log('Moving images source to build directory');
   return gulp
     .src(config.images)
-    .pipe(gulp.dest(config.build + 'images'))
-    .pipe($.connect.reload());
+    .pipe(gulp.dest(config.build + 'images'));
 });
 
-gulp.task('html:dev', () => {
-  log('Moving HTML to build directory');
-  return gulp
-    .src(config.html)
-    .pipe(gulp.dest(config.dev))
-    .pipe($.connect.reload());
-});
-
-gulp.task('html', () => {
-  log('Moving HTML to build directory');
-  return gulp
-    .src(config.html)
-    .pipe(gulp.dest(config.build))
-    .pipe($.connect.reload());
-});
 
 gulp.task('webserver', () => {
   log('Running webserver and livereload');
-  $.connect.server({
-    // the root parameter is needed
-    root: ['dev'],
-    livereload: true,
-    port: 3000
-  });
+  gulp.src(config.root)
+    .pipe($.webserver({
+      livereload: true,
+      directoryListing: false,
+      open: true,
+      port: 3000
+    }));
 });
 
 gulp.task('watch', () => {
-  log('Listening file changes');
-  gulp.watch(config.html)
+  log('Listening to file changes');
   gulp.watch(config.js, ['scripts']);
   gulp.watch(config.scss, ['styles']);
-  gulp.watch(config.html, ['html']);
-});
-
-
-gulp.task('dev', () => {
-  runSequence('html:dev', 'images:dev', 'styles:dev', 'scripts:dev', 'watch', 'webserver');
 });
 
 gulp.task('default', () => {
-  runSequence('html', 'images', 'styles', 'scripts', 'watch', 'webserver');
+  runSequence('images', 'styles', 'scripts', 'watch', 'webserver');
 });
 
 // helper functions
